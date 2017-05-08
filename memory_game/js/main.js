cee688e025c29cf80f2e8721c6fcf31c8f808de2
bryanleejh <bryan.lee.jh@gmail.com>
2017-05-08 14:34:34 +0800

    Update main,js

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,21 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
+
+var array = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];	
+var cardOne = array[0];
+cardsInPlay.push(cardOne);
+
+console.log("User flipped " + cardsInPlay[0]);
+
+var cardTwo = array[2];
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardsInPlay[1]);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
